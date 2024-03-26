e461b85090b2572590e1e5d0362ffa2c723e2509
Alejandra <alejandraguayo@outlook.es>
2024-03-26 18:16:08 -0300

    pom.xml

--- src/test/java/cursoqa/clase8/HomeTest.java
+++ src/test/java/cursoqa/clase8/HomeTest.java
@@ -1,46 +1,35 @@
 package cursoqa.clase8;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
-
-public class HomeTest {
-    private WebDriver driver;
-
-    @BeforeMethod
-    public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start.maximized");
-        options.addArguments("incognito");
-
-
-        options.setPageLoadTimeout(Duration.ofSeconds(60));
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
-
-        this.driver = new ChromeDriver(options);
-    }
-
+public class HomeTest extends BaseTest {
     @Test
     public void validarHome() {
-        //Paso 1: Levantar la página
-        this.driver.get("https://opencart.abstracta.us/");
-        // Paso 2: Realizar las aserciones
+
+        // Paso 1: levantar la pagina
+        getDriver().get("https://opencart.abstracta.us/");
+
+        // Paso 2: realizar las aserciones
         By inputBy = By.name("search");
-        By inputBy2 = By.xpath("// input[@name= ' search ']]");
+        By carruselBy = By.id("content");
+        By menuBy = By.id("menu");
+        By myAccountBy = By.xpath("//a[@title= \' My Account \' ] ");
 
-    }
+        WebElement inputEl = getDriver().findElement(inputBy);
+        WebElement carruselEl = getDriver().findElement(carruselBy);
+        WebElement menuEl = getDriver().findElement(menuBy);
+        WebElement myAccountEl = getDriver().findElement(myAccountBy);
 
-    @AfterMethod
-    public void tearDown() {
-        if (this.driver != null) {
-            this.driver.quit();
 
-        }
+
+        Assert.assertTrue(inputEl.isDisplayed());
+        Assert.assertTrue(carruselEl.isDisplayed());
+        Assert.assertTrue(menuEl.isDisplayed());
+        Assert.assertTrue(myAccountEl.isDisplayed());
     }
 }
+
+
+
+
---
+++ src/test/java/cursoqa/clase8/LoginTest.java
@@ -0,0 +1,40 @@
+package cursoqa.clase8;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class LoginTest extends BaseTest {
+    @Test
+    public void loginTest() {
+        // Paso 1: levantar la pagina
+        getDriver().get("https://opencart.abstracta.us/");
+
+        //Paso 2: click en My Account
+        By myAccountBy = By.xpath("//a[@title= \' My Account \' ] ");
+        WebElement myAccountEl = getDriver().findElement(myAccountBy);
+        myAccountEl.click();
+
+        //Paso 3: click en el dropdown
+        By loginBy = By.xpath("//a[text()=\"Login\"]");
+        WebElement loginEl = getDriver().findElement(loginBy);
+        loginEl.click();
+
+        // Paso 4: hacer login(ingresar user, pass y click en boton login)
+        By inputEmailBy = By.id("input-email");
+        By inputPassBy = By.id("input-password");
+        By buttonLoginBy = By.xpath("//input[@value=\' Login \']");
+
+        WebElement inputEmailEl = getDriver().findElement(inputEmailBy);
+        WebElement inputPassEl= getDriver().findElement(inputPassBy);
+        WebElement buttonLoginEl = getDriver().findElement(buttonLoginBy);
+        inputEmailEl.sendKeys("alejandraguayo@outlook.es");
+        inputPassEl.sendKeys("$pYJwn3mpnZmr9");
+        buttonLoginEl.click();
+
+        //Paso 5
+        By titleBy = By.xpath("//h2[text()=\'My Account\' ]" );
+        WebElement titleEl = getDriver().findElement(titleBy);
+        Assert.assertTrue(titleEl.isDisplayed());
+
+    }
+}
